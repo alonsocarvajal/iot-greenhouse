da0c900923a61948611bb882a0da412a366d906c
Alonso Carvajal <alonso@Alonsos-MacBook-Air.local>
2017-12-15 11:32:14 -0300

    Testing moment-timezone library

--- models/record.js
+++ models/record.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+var moment = require('moment-timezone');
+
 
 var Schema = mongoose.Schema;
 
 var RecordSchema = new Schema(
     {
-        date: {type: Date, default: Date.now},
+        date: {type: Date, default: moment().tz("America/Santiago").format()},
         _sensorId: {type: Schema.Types.ObjectId, required: true},        
         temperature: {type: Number, required: true},
         humidity: {type: Number, required: true}
